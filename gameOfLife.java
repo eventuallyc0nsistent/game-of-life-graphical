b72a0c38912bbb0aeff61fe07982e734e1be7a67
kirankoduru <crazycreature11@gmail.com>
2013-10-24 02:42:12 -0400

    grid created with highlighting buttons

--- gameOfLife.java
+++ gameOfLife.java
@@ -1,10 +1,11 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 class gameOfLife
 {	
-	private static int rows ;
-	private static int cols ;
+	private static int rows=0 ;
+	private static int cols=0 ;
 
 	public static void main(String args[])
 	{
@@ -12,6 +13,20 @@ class gameOfLife
 		{
 			rows = Integer.parseInt(args[0]) ;
 			cols = Integer.parseInt(args[1]) ;
+
+			//create frame
+			JFrame frame = new GameFrame();
+			
+			// for grid panel set layout to a Grid Layout
+			JPanel grid_panel = new JPanel();
+			grid_panel.setLayout(new GridLayout(rows,cols));
+
+			// make a grid of buttons  and add buttons to the emptyGrid
+			MakeGrid gameGrid = new MakeGrid();
+			JPanel emptyGrid = gameGrid.createEmptyGridOfButtons(rows,cols,grid_panel);
+
+			frame.add(emptyGrid);
+			frame.setVisible(true);
 		} 
 		else 
 		{
@@ -19,21 +34,20 @@ class gameOfLife
 			System.exit(0);
 		}
 
-		JFrame frame =new gameFrame();
-		frame.setVisible(true);
-
 	}
+
 }
 
-class gameFrame extends JFrame
+class GameFrame extends JFrame
 {
-	public gameFrame()
+	public GameFrame()
 	{
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		setTitle("Game of Life - Graphical");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JPanel menu_panel = new JPanel();
+
 		JButton next_btn = new JButton("Next");
 		JButton clear_btn = new JButton("Clear");
 
@@ -41,8 +55,66 @@ class gameFrame extends JFrame
 		menu_panel.add(clear_btn);
 
 		add(menu_panel, BorderLayout.NORTH);
+
 	}
 
 	public static final int DEFAULT_HEIGHT = 200;
 	public static final int DEFAULT_WIDTH = 300;
+}
+
+/*
+make a grid
+ */
+class MakeGrid
+{
+
+	/*
+	Create grid of JButtons
+	 */
+	public JPanel createGridOfButtons(int rows, int cols, JPanel grid_panel)
+	{
+		JButton[][] grid = new JButton[rows][cols];
+
+		for (int i=0; i < rows; i++)
+		{
+			for(int j=0 ; j < cols ; j++)
+			{
+				grid[i][j] = new JButton();
+				grid[i][j].setBackground(Color.WHITE);
+				grid_panel.add(grid[i][j]);
+			}
+		}
+
+		return grid_panel;
+	}
+
+	public JPanel createEmptyGridOfButtons(int rows, int cols, JPanel grid_panel)
+	{
+		JButton[][] grid = new JButton[rows][cols];
+		ActionListener buttonListener = new GridButtonListener();
+
+		for (int i=0; i < rows; i++)
+		{
+			for(int j=0 ; j < cols ; j++)
+			{
+				grid[i][j] = new JButton();
+				grid[i][j].setBackground(Color.WHITE);
+				grid[i][j].addActionListener(buttonListener);
+				grid_panel.add(grid[i][j]);
+			}
+		}
+
+		return grid_panel;
+	}
+}
+
+class GridButtonListener implements ActionListener
+{
+	public void actionPerformed(ActionEvent e)
+	{
+		if(((JButton)e.getSource()).getBackground() == Color.WHITE)
+			((JButton)e.getSource()).setBackground(Color.BLACK);
+		else
+			((JButton)e.getSource()).setBackground(Color.WHITE);
+	}
 }
